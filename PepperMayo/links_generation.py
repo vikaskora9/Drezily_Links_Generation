d67345b08ff887e00cc2b771a12a50a0360976d2
vikaskora <vikas.kora@artyvis.ai>
2025-05-20 07:55:52 +0530

    updated peppermayo

--- PepperMayo/links_generation.py
+++ PepperMayo/links_generation.py
@@ -8,8 +8,8 @@ import pandas as pd
 # filter.tags_type=Maxi%20Dresses&
 #
 # bgfilter.collection_handle=dresses&redirectResponse=full&ajaxCatalog=Snap&resultsFormat=native
-upd_url = 'https://5crv30.a.searchspring.io/api/search/search.json?userId=77cb8fe7-64a9-478a-9740-bdcbfcf0a632&domain=https%3A%2F%2Fpeppermayo.com%2Fcollections%2Fdresses&sessionId=1b5d6fc5-5d56-4582-a2e5-e91464fde3fa&pageLoadId=87a9ca92-70a6-43df-90c0-0f4ad4872a39&siteId=5crv30&bgfilter.collection_handle=dresses&redirectResponse=full&ajaxCatalog=Snap&resultsFormat=native'
-resp = json.loads(requests.get('https://5crv30.a.searchspring.io/api/search/search.json?userId=77cb8fe7-64a9-478a-9740-bdcbfcf0a632&domain=https%3A%2F%2Fpeppermayo.com%2Fcollections%2Fdresses&sessionId=1b5d6fc5-5d56-4582-a2e5-e91464fde3fa&pageLoadId=87a9ca92-70a6-43df-90c0-0f4ad4872a39&siteId=5crv30&bgfilter.collection_handle=dresses&redirectResponse=full&ajaxCatalog=Snap&resultsFormat=native').text)
+upd_url =  'https://5crv30.a.searchspring.io/api/search/search.json?userId=77cb8fe7-64a9-478a-9740-bdcbfcf0a632&domain=https%3A%2F%2Fasia.peppermayo.com%2Fcollections%2Ftops&sessionId=d559d464-a88f-4b45-8b62-dcfdb39bd285&pageLoadId=77fa727b-b393-4860-a7c4-bc5502caac6b&siteId=5crv30&bgfilter.collection_handle=tops&ajaxCatalog=Snap&resultsFormat=native'
+resp = json.loads(requests.get('https://5crv30.a.searchspring.io/api/search/search.json?userId=77cb8fe7-64a9-478a-9740-bdcbfcf0a632&domain=https%3A%2F%2Fasia.peppermayo.com%2Fcollections%2Ftops&sessionId=d559d464-a88f-4b45-8b62-dcfdb39bd285&pageLoadId=77fa727b-b393-4860-a7c4-bc5502caac6b&siteId=5crv30&bgfilter.collection_handle=tops&ajaxCatalog=Snap&resultsFormat=native').text)
 
 
 df, ctr = pd.DataFrame(), 0
@@ -19,12 +19,12 @@ for attr in resp['facets']:
         df.at[ctr, 'Category'] = 'Women'
         df.at[ctr, 'Product_Category'] = 'Clothing'
         df.at[ctr, 'Subcategory1'] = 'Western'
-        df.at[ctr, 'Subcategory2'] = 'Dresses and Jumpsuits'
-        df.at[ctr, 'Subcategory3'] = 'Dresses'
+        df.at[ctr, 'Subcategory2'] = 'Topwear'
+        df.at[ctr, 'Subcategory3'] = 'Tops'
         df.at[ctr, 'Url'] = upd_url.replace('siteId=5crv30&', f"siteId=5crv30&%23%2Ffilter%3A{attr['field']}%3A{attr_value['value']}&").replace('bgfilter.collection_handle', f'filter.{attr["field"]}={attr_value["value"]}&bgfilter.collection_handle')
         df.at[ctr, 'Attr_Name'] = attr['label']
         
         df.at[ctr, 'Attr_Value'] = attr_value['value']
         ctr += 1
 
-df.to_csv('pinkblush_dreeses_filters.csv', index=False, index_label=False)
+df.to_csv('pinkblush_toops_filters.csv', index=False, index_label=False)
